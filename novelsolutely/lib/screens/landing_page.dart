792e3791589a7e2935762a8e6aa2bf8c44188278
Hele <bloodthirsty.snow.white@gmail.com>
2020-04-17 16:14:02 +0200

    drawer-functional

--- novelsolutely/lib/screens/landing_page.dart
+++ novelsolutely/lib/screens/landing_page.dart
@@ -1,13 +1,7 @@
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:novelsolutely/models/project.dart';
-import 'package:novelsolutely/widgets/dialog_project.dart';
 import 'package:provider/provider.dart';
-import 'package:uuid/uuid.dart';
-
 import '../providers/logged-user.dart';
+import '../widgets/drawer_widget.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -15,12 +9,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-//  var uid = Provider.of<AuthService>(context, listen: false).getUId();
-  var _uid = 'FcUE8ntyTkZWD2Juu1dklakN67t2';
-  final _databaseReference = Firestore.instance;
-
+  Size _mediaQuery;
   @override
   Widget build(BuildContext context) {
+    _mediaQuery = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: FittedBox(
@@ -40,105 +32,17 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
-      drawer: _projectWidget(),
+      drawer: DrawerWidget(),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
+          width: _mediaQuery.width,
+          height: _mediaQuery.height,
           color: Colors.amber,
           child: Wrap(
             children: <Widget>[],
           ),
         ),
-      ),
-    );
-  }
-
-  void _writeProject(String title) async {
-    var projectUid = Uuid().v1();
-
-    var docTitle = title.replaceAll(new RegExp(r'[^a-zA-Z0-9]'), '-');
-    docTitle = docTitle.replaceAll(new RegExp(r'-{2,}'), '-');
-    await _databaseReference
-        .collection('users')
-        .document('$_uid')
-        .collection('projects')
-        .document(docTitle)
-        .setData({'title': title, 'id': '$projectUid'});
-  }
-
-  _getProjectsWidget(AsyncSnapshot<QuerySnapshot> snapshot) {
-    return snapshot.data.documents.map((doc) {
-      Project project = Project.fromFirestore(doc);
-      return ListTile(
-        title: new Text(project.title),
-        leading: project.imageUrl.isEmpty
-            ? CircleAvatar(
-                backgroundColor: Theme.of(context).primaryColor,
-                child: AutoSizeText(
-//                  '${project.title[0]}',
-                  '${project.title.substring(0,2)}',
-                  style: TextStyle(color: Colors.white, fontFamily: 'Pacifico'),
-                ),
-              )
-            : CircleAvatar(
-                backgroundImage: NetworkImage(project.imageUrl),
-              ),
-      );
-    }).toList();
-  }
-
-  _projectWidget() {
-    return Container(
-      width: 300.0,
-      color: Colors.white,
-      child: Column(
-        children: <Widget>[
-          DrawerHeader(
-            child: Container(
-              width: 300.0,
-              child: Container(),
-            ),
-            decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-            ),
-          ),
-          StreamBuilder(
-              stream: _databaseReference
-                  .collection('users')
-                  .document('$_uid')
-                  .collection('projects')
-                  .orderBy('title')
-                  .snapshots(),
-              builder: (_, AsyncSnapshot<QuerySnapshot> snapshot) {
-                if (!snapshot.hasData) {
-                  return Padding(
-                    padding: const EdgeInsets.only(top: 28.0),
-                    child: SpinKitThreeBounce(
-                      color: Colors.white,
-                      size: 20.0,
-                    ),
-                  );
-                } else {
-                  return Expanded(
-//                    height: 300,
-                    child: ListView(
-                      children: _getProjectsWidget(snapshot),
-                    ),
-                  );
-                }
-              }),
-          ListTile(
-            title: Text('Nuevo proyecto'),
-            leading: Icon(Icons.add),
-            onTap: () async {
-              var projectName = await showDialog(
-                  context: context, builder: (_) => DialogNewProject());
-              if (projectName != null) _writeProject(projectName);
-            },
-          )
-        ],
       ),
     );
   }
---
+++ novelsolutely/lib/widgets/drawer_widget.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:uuid/uuid.dart';
+
+import '../models/project.dart';
+import './dialog_project.dart';
+
+class DrawerWidget extends StatefulWidget {
+  @override
+  _DrawerWidgetState createState() => _DrawerWidgetState();
+}
+
+class _DrawerWidgetState extends State<DrawerWidget> {
+  //  var uid = Provider.of<AuthService>(context, listen: false).getUId();
+  var _uid = 'FcUE8ntyTkZWD2Juu1dklakN67t2';
+  final _databaseReference = Firestore.instance;
+  ThemeData _theme;
+
+  @override
+  Widget build(BuildContext context) {
+    _theme = Theme.of(context);
+    return Container(
+      width: 300.0,
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          DrawerHeader(
+            child: Container(
+              width: 300.0,
+              child: Container(),
+            ),
+            decoration: BoxDecoration(
+              color: _theme.primaryColor,
+            ),
+          ),
+          StreamBuilder(
+              stream: _databaseReference
+                  .collection('users')
+                  .document('$_uid')
+                  .collection('projects')
+                  .orderBy('title')
+                  .snapshots(),
+              builder: (_, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.only(top: 28.0),
+                    child: SpinKitThreeBounce(
+                      color: _theme.primaryColor,
+                      size: 20.0,
+                    ),
+                  );
+                } else {
+                  return Expanded(
+//                    height: 300,
+                    child: ListView(
+                      children: _getProjectsWidget(snapshot),
+                    ),
+                  );
+                }
+              }),
+          ListTile(
+            title: Text('Nuevo proyecto'),
+            leading: Icon(Icons.add),
+            onTap: () async {
+              var projectName = await showDialog(
+                  context: context, builder: (_) => DialogNewProject());
+              if (projectName != null) _writeProject(projectName);
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  _getProjectsWidget(AsyncSnapshot<QuerySnapshot> snapshot) {
+    return snapshot.data.documents.map((doc) {
+      Project project = Project.fromFirestore(doc);
+      return ListTile(
+        title: new Text(project.title),
+        leading: project.imageUrl.isEmpty
+            ? CircleAvatar(
+                backgroundColor: _theme.primaryColor,
+                child: AutoSizeText(
+                  '${project.title.substring(0, 2)}',
+                  style: TextStyle(color: Colors.white, fontFamily: 'Pacifico'),
+                ),
+              )
+            : CircleAvatar(
+                backgroundImage: NetworkImage(project.imageUrl),
+              ),
+      );
+    }).toList();
+  }
+
+  void _writeProject(String title) async {
+    var projectUid = Uuid().v1();
+
+    var docTitle = title.replaceAll(new RegExp(r'[^a-zA-Z0-9]'), '-');
+    docTitle = docTitle.replaceAll(new RegExp(r'-{2,}'), '-');
+    await _databaseReference
+        .collection('users')
+        .document('$_uid')
+        .collection('projects')
+        .document(docTitle)
+        .setData({'title': title, 'id': '$projectUid'});
+  }
+}
