5f9808cb018fe32aaca8220391927241935ffd69
TheHolyDenier <bloodthirsty.snow.white@gmail.com>
2020-09-13 11:51:21 +0200

    cleaned

--- novelsolutely/android/app/src/main/kotlin/com/example/novelsolutely/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.novelsolutely
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- novelsolutely/lib/models/user.dart
+++
@@ -1,7 +0,0 @@
-class User {
-  String id;
-  String name;
-  String imageUrl;
-
-  User({this.id, this.name, this.imageUrl});
-}
